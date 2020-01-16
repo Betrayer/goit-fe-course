705f17d7b798517cf359640c44e4e870ce78d976
Betrayer <StarDestroyer@ukr.net>
2020-01-16 16:52:21 +0200

    one more test

---
+++ test/src/js/animation.js
@@ -0,0 +1,49 @@
+$('#to-header').on('click', function() {
+    const images = $('#header').position().top;
+    
+    $('html, body'). animate(
+    {
+        scrollTop: images
+    }, 
+    900);
+}) ;
+
+$('#to-features').on('click', function() {
+    const images = $('#features').position().top;
+    
+    $('html, body'). animate(
+    {
+        scrollTop: images
+    }, 
+    900);
+}) ;
+
+$('#to-team').on('click', function() {
+    const images = $('#team').position().top;
+    
+    $('html, body'). animate(
+    {
+        scrollTop: images
+    }, 
+    900);
+}) ;
+
+$('#to-customers').on('click', function() {
+    const images = $('#customers').position().top;
+    
+    $('html, body'). animate(
+    {
+        scrollTop: images
+    }, 
+    900);
+}) ;
+
+$('#to-contact').on('click', function() {
+    const images = $('#contact').position().top;
+    
+    $('html, body'). animate(
+    {
+        scrollTop: images
+    }, 
+    900);
+}) ;
